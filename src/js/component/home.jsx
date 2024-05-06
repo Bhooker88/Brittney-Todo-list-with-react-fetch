da8fdf2ba5434a5a633db0dbd09cbd5af62eea38
Brittney Hooker <139884664+Bhooker88@users.noreply.github.com>
2024-05-06 23:10:41 +0000

    updated for new API

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,44 +1,70 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { getallTodos, updateTodos } from "./todoList";
+import React, { useEffect, useState } from "react";
+import { getallTodos, createTodo, updateTodo, deleteTodo, clearAllTodos } from "./todoList";
 
-
-const localStorageKey = "Todos_key";
 const Home = () => {
   const [Todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState("");
-  const [previousToDo, setPreviousToDo] = useState(Todos);
+  const userName = "bhooker";
 
-  useEffect(async () => {
-    console.log("Run it");
-    console.log(localStorage.getItem(localStorageKey));
 
-    let apiTodos = await getallTodos();
-    setTodos(apiTodos);
-    setPreviousToDo(apiTodos);
-  }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+        let apiTodos = await getallTodos();
+        apiTodos = apiTodos.map(todo => ({ ...todo, done: todo.done ?? false }));
+        setTodos(apiTodos);
+    };
 
-  useEffect(async () => {
-    console.log("Everytime To do changes");
-    console.log(JSON.stringify(Todos));
-   
-    let apiUpdatesTodos = await updateTodos(Todos);
-  }, [Todos]);
+    fetchData();
+}, []);
 
-  let onType = (event) => {
-    console.log(event);
-    if (event.key == "Enter") {
-      let newTodos = [...Todos];
-      newTodos.push({ label: event.target.value, done: false });
-      setTodos(newTodos);
-      setPreviousToDo(Todos);
-      
-      event.target.value = "";
-    } else {
-      setInputValue(event.target.value);
-      console.log(event);
+  const handleCreateTodo = async (label) => {
+    const newTodo = await createTodo(userName, { label: label, is_done: false });
+    if (newTodo) {
+      setTodos(prev => [...prev, newTodo]);
     }
   };
+
+  const onType = (event) => {
+    setInputValue(event.target.value);
+    if (event.key === "Enter" && event.target.value.trim() !== "") {
+      handleCreateTodo(event.target.value);
+      setInputValue("");
+    }
+  };
+
+  const toggleTodoDone = async (index) => {
+    const updatedTodos = [...Todos];
+    updatedTodos[index] = {
+        ...updatedTodos[index],
+        done: !updatedTodos[index].done
+    };
+    try {
+        const response = await updateTodo(updatedTodos[index].id, updatedTodos[index]);
+        if (response) {
+            console.log('Updated todo:', response);
+            setTodos(updatedTodos);
+        } else {
+            console.error("API call was successful, but no response was returned.");
+        }
+    } catch (error) {
+        console.error('Failed to update todo:', error);
+    }
+};
+
+  const handleDeleteTodo = async (index) => {
+    const todoId = Todos[index].id;
+    if (await deleteTodo(todoId)) {
+      let newTodos = [...Todos];
+      newTodos.splice(index, 1);
+      setTodos(newTodos);
+    }
+  };
+
+  const handleClearAll = async () => {
+    await clearAllTodos(userName);
+    setTodos([]);
+  };
+
   return (
     <div className="todo-container">
       <h1 className="todo-title">Todo List</h1>
@@ -47,59 +73,35 @@ const Home = () => {
           className="todo-input"
           onKeyUp={onType}
           placeholder="Enter Todo"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
         />
       </div>
       <ul className="todo-ul">
-        {Todos.map((todo, index) => {
-          return (
-            <li className="todo-item" key={index}>
-              <input
-                className="todo-checkbox"
-                type="checkbox"
-                checked={todo.done}
-                onChange={() => {
-                  let newTodos = [...Todos];
-                  newTodos[index].done = !todo.done;
-                  setTodos(newTodos);
-                  setPreviousToDo(Todos);
-                }}
-              />
-              <p className="todo-label">{todo.label}</p>
-              {}
-              <button
-                className="todo-delete-item"
-                onClick={() => {
-                  let newTodos = [...Todos];
-                  newTodos.splice(index, 1);
-                  setTodos(newTodos);
-                  setPreviousToDo(Todos);
-                }}
-              >
-                🗑️
-              </button>
-            </li>
-          );
-        })}
+        {Todos.map((todo, index) => (
+          <li className="todo-item" key={index}>
+            <input
+              className="todo-checkbox"
+              type="checkbox"
+              checked={todo.done}
+              onChange={() => toggleTodoDone(index)}
+            />
+            <p className="todo-label">{todo.label}</p>
+            <button
+              className="todo-delete-item"
+              onClick={() => handleDeleteTodo(index)}
+            >
+              🗑️
+            </button>
+          </li>
+        ))}
       </ul>
       <div className="todo-footer">
         <p className="todo-items-left">{Todos.length} item(s) left</p>
-        <button
-          className="todo-undo-button"
-          onClick={() => {
-            
-            setTodos(previousToDo);
-          }}
-        >
+        <button className="todo-undo-button" onClick={() => setTodos([...Todos])}>
           Undo
         </button>
-
-        <button
-          className="todo-clearall-button"
-          onClick={() => {
-            setTodos([]);
-            setPreviousToDo(Todos);
-          }}
-        >
+        <button className="todo-clearall-button" onClick={handleClearAll}>
           Clear All
         </button>
       </div>
--- src/js/component/todoList.js
+++ src/js/component/todoList.js
@@ -1,37 +1,67 @@
-
-
 async function getallTodos(){
-    await fetch('https://playground.4geeks.com/apis/fake/todos/user/bhooker', {
-       method: "POST",
-       body: JSON.stringify([]),
-       headers: {
-         "Content-Type": "application/json"
-       },
-})
-   const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/bhooker', {
-       method: "GET"
-       ,
-       headers: {
-         "Content-Type": "application/json"
-       }
-})
-const data = await response.json()
-console.log(data);
-
-return data;
-
-}
-async function updateTodos(abc) {
-
-  const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/bhooker', {
-       method: "PUT",
-       body: JSON.stringify(abc),
-       headers: {
-         "Content-Type": "application/json"
-       },
-})
-console.log(response)
-
+  const response = await fetch('https://playground.4geeks.com/todo/users/bhooker', {
+      method: "GET",
+      headers: {
+          "Content-Type": "application/json"
+      }
+  });
+  const data = await response.json();
+  console.log(data);
+  return data.todos.map(todo => ({
+      label: todo.label,
+      done: todo.is_done,
+      id: todo.id
+  }));
 }
 
-export {getallTodos, updateTodos};
+async function createTodo(userName, todo) {
+  const response = await fetch(`https://playground.4geeks.com/todo/todos/${userName}`, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+          label: todo.label,
+          is_done: todo.done
+      })
+  });
+  const responseData = await response.json();
+  console.log(responseData);
+  return responseData;
+}
+
+async function updateTodo(todoId, todo) {
+  const response = await fetch(`https://playground.4geeks.com/todo/todos/${todoId}`, {
+      method: "PUT",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+          label: todo.label,
+          is_done: todo.done
+      })
+  });
+  const responseData = await response.json();
+  console.log(responseData);
+  return responseData;
+}
+
+async function deleteTodo(todoId) {
+  const response = await fetch(`https://playground.4geeks.com/todo/todos/${todoId}`, {
+      method: "DELETE",
+      headers: {
+          "Content-Type": "application/json"
+      }
+  });
+  console.log(`Deleted Todo ID: ${todoId}`);
+  return response.ok; 
+}
+
+async function clearAllTodos(userName) {
+  // Retrieve all todos to clear them
+  const todos = await getallTodos(userName);
+  await Promise.all(todos.map(todo => deleteTodo(todo.id))); 
+  console.log("All todos cleared.");
+}
+
+export { getallTodos, createTodo, updateTodo, deleteTodo, clearAllTodos };
